2e06f658b7190fce74e813d8ae2aa96dcc48a44b
D.J. Denton <ddenton@uoguelph.ca>
2021-10-07 20:25:16 -0400

    Disable enemies giving or taking damage.

--- Assets/Scripts/Mechanics/Health.cs
+++ Assets/Scripts/Mechanics/Health.cs
@@ -13,7 +13,7 @@ namespace Platformer.Mechanics
         /// <summary>
         /// The maximum hit points for the entity.
         /// </summary>
-        public int maxHP = 100;
+        public int maxHP = 1;
 
         /// <summary>
         /// Indicates if the entity should be considered 'alive'.
