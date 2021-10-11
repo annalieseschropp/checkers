39e26f3f6395b6f1f6e727e0abb746fb452e0636
unknown <a.schropp@hotmail.com>
2021-10-10 22:02:37 -0400

    Created visual instance of board, including alternating colours

--- Assets/Assets/Scripts/Board.cs
+++ Assets/Assets/Scripts/Board.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject blackTilePrefab;
+    public GameObject whiteTilePrefab;
+    private Tile[,] boardTiles;
+
     void Start()
     {
-        
+        boardTiles = new Tile[8, 8]; // Create new instance of 8x8 board
+        Create();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Create()
     {
-        
+        bool isBlack = true;
+        for (int x = 0; x < 8; x++) // X Axis
+        {
+            for (int y = 0; y < 8; y++) // Y Axis
+            {
+                Vector2 tempPosition = new Vector2(x, y);
+                if (isBlack) // If black then instantiate a black tile object
+                {
+                    Instantiate(blackTilePrefab, tempPosition, Quaternion.identity);
+                } else // If not black then instantiate a white tile object
+                {
+                    Instantiate(whiteTilePrefab, tempPosition, Quaternion.identity);
+                }
+                isBlack = !isBlack;
+            }
+            isBlack = !isBlack; // Offset next column to ensure checkboard pattern
+        }
     }
 }
--- Assets/Assets/Scripts/Tile.cs
+++ Assets/Assets/Scripts/Tile.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dummyScript : MonoBehaviour
+public class Tile : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
