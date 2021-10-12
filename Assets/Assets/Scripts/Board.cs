c95a72e3a26783ddaac5fb8e49238edf0080940e
unknown <a.schropp@hotmail.com>
2021-10-11 23:59:20 -0400

    Added state namespace for enum, added setter and getter for board state

--- Assets/Assets/Scripts/Board.cs
+++ Assets/Assets/Scripts/Board.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CheckersState;
 
 /// <summary>
 /// Class
@@ -10,33 +11,25 @@ public class Board : MonoBehaviour
 {
     public GameObject blackTilePrefab;
     public GameObject whiteTilePrefab;
-
-    /// <summary>
-    /// Enum
-    /// Holds the state of each tile.
-    /// </summary>
-    private enum State {
-        Empty = 0,
-        White = 1,
-        Black = 2,
-        WhiteKing = 3,
-        BlackKing = 4
-    };
+    public CheckersState.State[,] curState;
 
     /// <summary>
     /// Constructor of board class.
     /// </summary>
     void Start()
     {
+        curState = new CheckersState.State[8, 8];
         Create();
+        setInitBoardState();
+        Debug.Log(getBoardState()[0,0]);
     }
 
     /// <summary>
     /// Method
     /// Creates instance of board and populates with alternating coloured tiles.
+    /// </summary>
     /// <params>None</params>
     /// <returns>Void</returns>
-    /// </summary>
     private void Create()
     {
         bool isBlack = true;
@@ -48,7 +41,7 @@ public class Board : MonoBehaviour
         {
             for (int y = 0; y < 8; y++) // Y Axis
             {
-                Vector2 tempPosition = new Vector2(x, y);
+                Vector3 tempPosition = new Vector3(x, y, -1);
                 if (isBlack) // If black then instantiate a black tile object
                 {
                     tile = Instantiate(blackTilePrefab, tempPosition, Quaternion.identity) as GameObject;
@@ -62,5 +55,34 @@ public class Board : MonoBehaviour
             }
             isBlack = !isBlack; // Offset next column to ensure checkboard pattern
         }
+    }
+
+    /// <summary>
+    /// Method
+    /// Setter to populate current state array for each position on the board with an empty state 
+    /// as there are no pieces on the board yet.
+    /// </summary>
+    /// <params>None</params>
+    /// <returns>Void</returns>
+    private void setInitBoardState()
+    {
+        for (int x = 0; x < 8; x++)
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                curState[x, y] = CheckersState.State.Empty;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method
+    /// Getter for the overall board state
+    /// </summary>
+    /// <params>None</params>
+    /// <returns>curState - 2D array of State enums</returns>
+    public CheckersState.State[,] getBoardState()
+    {
+        return curState;
     }
 }
