9b6b8aada94b500fe5046a9a464f25fb0acd35c6
D.J. Denton <ddenton@uoguelph.ca>
2021-10-26 18:57:32 -0400

    add Square struct

--- Assets/Assets/Scripts/Move.cs
+++ Assets/Assets/Scripts/Move.cs
@@ -1,4 +1,11 @@
 namespace CheckersMove
 {
-   
+    /// <summary>
+    /// Struct
+    /// Holds a representation of a single square (just and (x,y) coordinate).
+    /// </summary>
+    public struct Square {
+        int x;
+        int y;
+    }
 }
