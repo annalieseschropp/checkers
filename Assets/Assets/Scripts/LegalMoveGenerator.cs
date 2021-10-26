6a8c6d34e2a334d540da920a4c93620231ce2cb4
D.J. Denton <ddenton@uoguelph.ca>
2021-10-26 19:03:57 -0400

    Create LegalMoveGenerator static class

---
+++ Assets/Assets/Scripts/LegalMoveGenerator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CheckersState;
+using CheckersMove;
+
+/// <summary>
+/// Class
+/// Models the 2D game board for checkers
+/// </summary>
+public static class LegalMoveGenerator
+{
+    
+}
--- Assets/Assets/Scripts/Move.cs
+++ Assets/Assets/Scripts/Move.cs
@@ -8,4 +8,13 @@ namespace CheckersMove
         int x;
         int y;
     }
+
+    /// <summary>
+    /// Struct
+    /// Holds a representation of a single move (src and dest square).
+    /// </summary>
+    public struct Move {
+        Square src;
+        Square dest;
+    }
 }
