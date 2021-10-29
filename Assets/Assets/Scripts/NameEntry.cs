a06c8681468df3e8262bfcd9ddc5343b7b16a11d
Sahibjeet-Babbrah <sahibjeetbabbrah@gmail.com>
2021-10-29 18:11:15 -0400

    Updated UI and code to allow for selecting previous players.

--- Assets/Assets/Scripts/NameEntry.cs
+++ Assets/Assets/Scripts/NameEntry.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class NameEntry : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class NameEntry : MonoBehaviour
     public Button cancelGameButton;
     public InputField playerOneName;
     public InputField playerTwoName;
+    public Dropdown dropdownPlayerOne;
+    public Dropdown dropdownPlayerTwo;
+    public Toggle forcedCapture;
     int update = 0;
 
     // Start is called before the first frame update
@@ -16,26 +20,68 @@ public class NameEntry : MonoBehaviour
     {
         Button playGame = playGameButton.GetComponent<Button>();
         Button cancelGame = cancelGameButton.GetComponent<Button>();
+        
+        dropdownPlayerOne = dropdownPlayerOne.GetComponent<Dropdown>();
+        dropdownPlayerTwo = dropdownPlayerTwo.GetComponent<Dropdown>();
+        forcedCapture = forcedCapture.GetComponent<Toggle>();
         playerOneName = playerOneName.GetComponent<InputField>();
         playerTwoName = playerTwoName.GetComponent<InputField>();
-    
+
+        List<Dropdown.OptionData> list = new List<Dropdown.OptionData>();
+        Dropdown.OptionData item1 = new Dropdown.OptionData();
+        item1.text = "Hello1";
+        list.Add(item1);
+        dropdownPlayerOne.AddOptions(list);
+
         playGame.onClick.AddListener(PlayGameButtonOnClick);
         cancelGame.onClick.AddListener(CancelGameButtonOnClick);
         playerOneName.onValueChanged.AddListener(delegate {PlayerOneNameOnUpdate();});
+        dropdownPlayerOne.onValueChanged.AddListener(delegate {PlayerOneDropdownOnUpdate();});
     }
 
     void PlayGameButtonOnClick()
     {
-        Debug.Log("Clicked + " + playerOneName.text);
+        Debug.Log("Clicked + " + forcedCapture.isOn);
     }
 
     void CancelGameButtonOnClick()
     {
-
+        SceneManager.LoadScene("Menu");
     }
 
+    // Ensure that only either a input value or a dropdown menu is selected
+
+    // Functions to ensure that when the entered name has a value, the dropdown is blank.
     void PlayerOneNameOnUpdate()
     {
-        
+        string value = playerOneName.text;
+        int position = playerOneName.caretPosition;
+        dropdownPlayerOne.value = 0;
+        playerOneName.text = value;
+        playerOneName.caretPosition = position;
+    }
+
+    void playerTwoNameOnUpdate()
+    {
+        string value = playerTwoName.text;
+        int position = playerTwoName.caretPosition;
+        dropdownPlayerTwo.value = 0;
+        playerTwoName.text = value;
+        playerTwoName.caretPosition = position;
+    }
+
+    // Functions to ensure that when the dropdown isn't blank, the input value is blank.
+    void PlayerOneDropdownOnUpdate()
+    {
+        int value = dropdownPlayerOne.value;
+        playerOneName.text = "";
+        dropdownPlayerOne.value = value;
+    }
+
+    void playerOneDropdownOnUpdate()
+    {
+        int value = dropdownPlayerOne.value;
+        playerTwoName.text = "";
+        dropdownPlayerTwo.value = value;
     }
 }
--- Assets/Assets/Scripts/NameStaticClass.cs
+++ Assets/Assets/Scripts/NameStaticClass.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+// using System.string;
 using UnityEngine;
 
 public class NameStaticClass : MonoBehaviour
 {
+    // Static variables to pass data between scenes
+    public static string playerOneName;
+    public static string playerTwoName;
+
     // Start is called before the first frame update
     void Start()
     {
