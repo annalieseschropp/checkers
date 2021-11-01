519a1831e0be5b1acf04d9a1ae4246114aec4dda
Ian McKechnie <imckechn@uoguelph.ca>
2021-11-01 18:16:59 -0400

    Added the functionality to the quit game feature

---
+++ Assets/Assets/Scripts/endGameScreen.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class endGameScreen : MonoBehaviour
+{
+
+    [SerializeField] Button _button1;
+    [SerializeField] Button _button2;
+    [SerializeField] Text _button1Text;
+    [SerializeField] Text _button2Text;
+    [SerializeField] Text _popupText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Assets/Scripts/popup.cs
+++ Assets/Assets/Scripts/popup.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class popup : MonoBehaviour
 {
+    public Button quitButtonInPopupWindow;
+    public Button goBackButton;
+    public GameObject popupPanel;
+    public Button quitButtonOnCheckBoard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Button quitBtn = quitButtonInPopupWindow.GetComponent<Button>();
+        quitBtn.onClick.AddListener(quitClicked);
+
+        Button goBackBtn = goBackButton.GetComponent<Button>();
+        goBackBtn.onClick.AddListener(closePopup);
+
+        Button quitGame = quitButtonOnCheckBoard.GetComponent<Button>();
+        quitGame.onClick.AddListener(openPopup);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void quitClicked()
     {
-        
+        //Close popup before leaving
+        popupPanel.SetActive(false);
+
+        //Now quit
+        Debug.Log("Quit Game");
+        SceneManager.LoadScene("Menu");
+    }
+
+    void closePopup()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    void openPopup()
+    {
+        popupPanel.SetActive(true);
     }
 }
