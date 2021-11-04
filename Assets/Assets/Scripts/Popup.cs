bec3da6b0e3facd301cfd5f58af2be34e5d25325
unknown <a.schropp@hotmail.com>
2021-11-03 20:52:57 -0400

    Added popup script

---
+++ Assets/Assets/Scripts/Popup.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Popup : MonoBehaviour
+{
+    public Button quitButtonInPopupWindow;
+    public Button goBackButton;
+    public GameObject popupPanel;
+    public Button quitButtonOnCheckBoard;
+
+    /// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+    void Start()
+    {
+        Button quitBtn = quitButtonInPopupWindow.GetComponent<Button>();
+        quitBtn.onClick.AddListener(quitClicked);
+
+        Button goBackBtn = goBackButton.GetComponent<Button>();
+        goBackBtn.onClick.AddListener(closePopup);
+
+        Button quitGame = quitButtonOnCheckBoard.GetComponent<Button>();
+        quitGame.onClick.AddListener(openPopup);
+
+    }
+
+    /// <summary>
+    /// Method
+    /// Closes the popup window and the current game
+    /// Update is called once per frame
+    /// </summary>
+    void QuitClicked()
+    {
+        //Close popup before leaving
+        popupPanel.SetActive(false);
+
+        //Now quit
+        Debug.Log("Quit Game");
+        SceneManager.LoadScene("Menu");
+    }
+
+
+    /// <summary>
+    /// Method
+    /// Closes the popup
+    /// </summary>
+    void ClosePopup()
+    {
+        popupPanel.SetActive(false);
+    }
+
+    /// <summary>
+    /// Method
+    /// Opens the popup
+    /// </summary>
+    void OpenPopup()
+    {
+        popupPanel.SetActive(true);
+    }
+}
