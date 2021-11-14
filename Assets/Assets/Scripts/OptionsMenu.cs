f3c479e5b7604f4dc0acd00c3afe6cff05cd93a0
D.J. Denton <ddenton@uoguelph.ca>
2021-11-14 18:54:07 -0500

    Add move speed slider.

--- Assets/Assets/Scripts/OptionsMenu.cs
+++ Assets/Assets/Scripts/OptionsMenu.cs
@@ -7,12 +7,21 @@ using UnityEngine.SceneManagement;
 public class OptionsMenu : MonoBehaviour
 {
     public Button backButton;
+    public Slider moveSpeedSlider;
 
     // Start is called before the first frame update
     void Start()
     {
         Button backBtn = backButton.GetComponent<Button>();
         backBtn.onClick.AddListener(GoBack);
+
+        Slider speedSlider = moveSpeedSlider.GetComponent<Slider>();
+        speedSlider.onValueChanged.AddListener(OnMoveSpeedSliderChanged);
+    }
+
+    public void OnMoveSpeedSliderChanged(float value)
+    {
+        Debug.Log("Slider Value" + value);
     }
 
     public void GoBack()
