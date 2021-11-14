5d525a11fcede6d91d61d143be3666694374dacb
D.J. Denton <ddenton@uoguelph.ca>
2021-11-14 16:55:40 -0500

    Add some tests for the end game popup.

---
+++ Assets/Assets/Tests/PlayMode/EndGamePopupTests.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using CheckersState;
+
+public class EndGamePopupTest
+{
+    [SetUp]
+    public void Setup()
+    {
+        SceneManager.LoadScene("GameBoard");
+    }
+
+    //Test the quit button
+    [UnityTest]
+    public IEnumerator QuitClickTest()
+    {
+        var control = GameObject.Find("/endGameElement").GetComponent(typeof(EndGamePopup)) as EndGamePopup;
+        control.quitIsClicked();
+        yield return new WaitForSeconds(1);
+        string scene = SceneManager.GetActiveScene().name;
+        yield return null;
+        Assert.AreEqual(true, scene == "Menu");
+    }
+
+    //Test the play again button
+    [UnityTest]
+    public IEnumerator PlayAgainClickTest()
+    {
+        var control = GameObject.Find("/endGameElement").GetComponent(typeof(EndGamePopup)) as EndGamePopup;
+        NameStaticClass.playerOneName = "Alice";
+        NameStaticClass.playerTwoName = "Bob";
+        control.restartGameFunc();
+        yield return new WaitForSeconds(1);
+        string scene = SceneManager.GetActiveScene().name;
+        yield return null;
+        Assert.AreEqual(true, scene == "GameBoard");
+        // Swap names
+        Assert.AreEqual("Alice", NameStaticClass.playerTwoName);
+        Assert.AreEqual("Bob", NameStaticClass.playerOneName);
+    }
+}
